ab47dbf2c3fb81251366a97b6c19624262a7126d
TANABE Ken-ichi <nabeken@tknetworks.org>
2020-03-19 00:03:10 +0900

    use negroni

--- cmd/capi/main.go
+++ cmd/capi/main.go
@@ -1,37 +1,45 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"net/url"
+	"os"
 
 	"github.com/aws/aws-sdk-go/aws/session"
 	v4signer "github.com/aws/aws-sdk-go/aws/signer/v4"
+	"github.com/fujiwara/ridge"
 	"github.com/nabeken/capi"
+	"github.com/urfave/negroni"
 )
 
 func main() {
-	s3Bucket := flag.String("s3-bucket", "", "S3 bucket name")
-	s3Region := flag.String("s3-region", "ap-northeast-1", "S3 bucket region")
+	region := os.Getenv("AWS_REGION")
+	if region == "" {
+		log.Fatal("Please set AWS_REGION environment variable.")
+	}
 
-	flag.Parse()
+	s3Bucket := os.Getenv("CAPI_S3_BUCKET")
+	if s3Bucket == "" {
+		log.Fatal("Please set CAPI_S3_BUCKET environment variable.")
+	}
 
 	s3Endpoint, err := url.Parse(
-		fmt.Sprintf("https://%s.s3-%s.amazonaws.com", *s3Bucket, *s3Region),
+		fmt.Sprintf("https://%s.s3-%s.amazonaws.com", s3Bucket, region),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	signer := &capi.Signer{
-		Region: *s3Region,
+		Region: region,
 		Signer: v4signer.NewSigner(session.Must(session.NewSession()).Config.Credentials),
 	}
 
 	rp := capi.NewProxy(signer, s3Endpoint)
-	http.Handle("/", rp)
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	n := negroni.Classic()
+	n.UseHandler(rp)
+
+	ridge.Run(":8080", "/", n)
 }
--- go.mod
+++ go.mod
@@ -3,3 +3,11 @@ module github.com/nabeken/capi
 go 1.13
 
 require github.com/aws/aws-sdk-go v1.29.25
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fujiwara/ridge v0.1.0
+	github.com/urfave/negroni v1.0.1-0.20191011213438-f4316798d5d3
+	golang.org/x/text v0.3.2 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
