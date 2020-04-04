4bb6e0700956ad7ba6c4342129b8d94e918e0ff5
TANABE Ken-ichi <nabeken@tknetworks.org>
2020-04-04 14:50:31 +0900

    writing tests

--- capi_test.go
+++ capi_test.go
@@ -1,6 +1,7 @@
 package capi
 
 import (
+	"errors"
 	"testing"
 	"time"
 
@@ -20,7 +21,6 @@ func (c *psMockClient) GetParameter(key string) (string, error) {
 }
 
 func TestS3EndpointResolver(t *testing.T) {
-	assert := assert.New(t)
 
 	cache := cache.New(time.Minute, time.Minute)
 	mockClient := &psMockClient{}
@@ -30,14 +30,52 @@ func TestS3EndpointResolver(t *testing.T) {
 		PSClient: mockClient,
 	}
 
-	expected := S3Endpoint{
-		Bucket: "test",
-		Region: "test",
-	}
+	t.Run("host with port", func(t *testing.T) {
+		assert := assert.New(t)
 
-	mockClient.Param = `{"bucket":"test","region":"test"}`
+		r.resolveEndpoint("example.com:8080")
+		assert.Equal("/capi/hosts/example.com_8080", mockClient.Arg)
+	})
 
-	actual, err := r.resolveEndpoint("example.com")
-	assert.NoError(err)
-	assert.Equal(expected, actual)
+	t.Run("success", func(t *testing.T) {
+		assert := assert.New(t)
+
+		expected := S3Endpoint{
+			Bucket: "test",
+			Region: "test",
+		}
+
+		mockClient.Param = `{"bucket":"test","region":"test"}`
+
+		actual, err := r.resolveEndpoint("example.com")
+		assert.NoError(err)
+		assert.Equal(expected, actual)
+
+		_, ok := cache.Get("__capi/error/example.com")
+		assert.False(ok)
+	})
+
+	t.Run("error", func(t *testing.T) {
+		assert := assert.New(t)
+
+		mockClient.Err = errors.New("not found")
+
+		actual, err := r.resolveEndpoint("example.com")
+		assert.EqualError(errors.Unwrap(err), mockClient.Err.Error())
+		assert.Equal(S3Endpoint{}, actual)
+
+		cachedIErr, cached := cache.Get("__capi/error/example.com")
+		assert.True(cached)
+
+		cachedErr, ok := cachedIErr.(error)
+		assert.True(ok)
+		assert.EqualError(cachedErr, mockClient.Err.Error())
+	})
+
+	t.Run("cached error", func(t *testing.T) {
+		assert := assert.New(t)
+
+		_, err := r.resolveEndpoint("example.com")
+		assert.Contains(err.Error(), "(cached)")
+	})
 }
